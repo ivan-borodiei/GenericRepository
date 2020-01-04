f73e95293d3987d7e4488d7eacb9c8495751c611
ivan-borodiei <ivan.borodiei@gmail.com>
2020-01-04 21:57:43 +0200

    Inline comment added

--- Console/Program.cs
+++ Console/Program.cs
@@ -6,7 +6,7 @@ namespace Console
     {
         static void Main(string[] args)
         {
-            RepositoryConsumer.RunTest();
+            RepositoryConsumer.RunTest(); //Run test
         }
     }
 }
